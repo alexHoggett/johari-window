1e3c03dcec0f2a53768857c2ff18e0cecb26b569
alexHoggett <alexmh99@gmail.com>
2023-04-01 00:09:03 +0100

    further project setup

---
+++ frontend/src/Adjective.test.ts
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import Adjective from './Adjective';
+
+describe('Adjective Component tests', () => {
+  it('renders on the page', () => {
+    render( <Adjective /> )
+  }) 
+})
