d2c304a4eaf4af4eafdea50a662574fd5c6e1dd1
alexHoggett <alexmh99@gmail.com>
2023-04-30 17:07:12 +0100

    styled error page

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import React from 'react'
 import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 const ErrorPage = () => {
@@ -8,23 +6,23 @@ const ErrorPage = () => {
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div>
-        <h1>Oops! {error.status}</h1>
-        <p>{error.statusText}</p>
+      <div className="flex flex-col gap-9 h-screen justify-center items-center">
+        <h1 className="text-center font-bold text-5xl">Oops!</h1>
+        <p className="italic flex items-center gap-4 text-lg"><span className='font-bold text-2xl'>{error.status}:</span> {error.statusText}</p>
         {error.data && (
           <p>
-            <i>{error.data}</i>
+            <i className="text-slate-300 text-lg">{error.data}</i>
           </p>
         )}
       </div>
     );
   } else if (error instanceof Error) {
     return (
-      <div>
-        <h1>Oops! Unexpected Error</h1>
-        <p>Something went wrong 😔</p>
+      <div className="flex flex-col gap-9 h-screen justify-center items-center">
+        <h1 className="text-center font-bold text-4xl">Oops! Unexpected Error</h1>
+        <p className="italic text-lg">Something went wrong 😔</p>
         <p>
-          <i>{error.message}</i>
+          <i className="text-slate-300 text-lg">{error.message}</i>
         </p>
       </div>
     );
