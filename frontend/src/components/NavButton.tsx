8d905493abd3892e215279dcda72184d344ac794
alexHoggett <alexmh99@gmail.com>
2023-04-30 22:14:32 +0100

    added hover effect to buttons

--- frontend/src/components/NavButton.tsx
+++ frontend/src/components/NavButton.tsx
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom'
+
 interface ButtonProps {
-  text: string,
-  destination: string,
+  children: string,
+  destination: string
 }
 
-const NavButton = ({text, destination}: ButtonProps) => {
+const NavButton = ({ children, destination }: ButtonProps) => {
   // Nice button here with hover effect
   return (
-    <></>
+    <Link 
+      type="button"
+      className="px-10 py-2 border text-lg rounded-full w-max text-center transition duration-200 hover:bg-orange-200 hover:text-black"
+      to={destination}
+    >
+      {children}
+    </Link>
   )
 }
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
+import NavButton from "../components/NavButton";
+
 const HomePage = () => {
   return (
-    <div className="p-12 flex flex-col gap-12">
+    <div className="p-12 flex flex-col gap-12 items-start w-min">
       <h1 className='text-9xl text-orange-200'>Johari <br/> Window</h1>
       <p className="max-w-lg text-xl"> 
       Unlock the mysteries of your mind and discover the hidden depths of your personality. Are you curious to explore the unknown parts of yourself that even you may not be aware of? Or perhaps you seek to understand the intricate dynamics of your relationships with others? Our application offers a unique and intriguing way to reveal the secrets of your true self and connect with others on a deeper level.
       </p>
+      <div className="flex justify-center items-center w-full gap-9">
+        <NavButton destination="/start">Start</NavButton>
+        <NavButton destination="#about">Learn More</NavButton>
+      </div>
     </div>
   )
 }
