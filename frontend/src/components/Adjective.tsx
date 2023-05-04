b870a5e9a1e67793ae1a252b8ec83473e8118768
alexHoggett <alexmh99@gmail.com>
2023-05-04 12:57:37 +0100

    completed django project setup and created the db

--- frontend/src/components/Adjective.tsx
+++ frontend/src/components/Adjective.tsx
@@ -2,15 +2,22 @@ import { useState } from 'react'
 import descriptions from './descriptions';
 
 interface AdjectiveProps {
-  text: string;
+  children: string;
 };
 
-function Adjective({ text }: AdjectiveProps): JSX.Element {
+function Adjective({ children }: AdjectiveProps): JSX.Element {
   const [selected, setSelected] = useState(false)
 
+  const handleClick = () => {
+    setSelected(!selected);
+  }
+
   return (
-    <button className="px-10 py-2 border text-lg rounded-full w-max text-center transition duration-200 hover:bg-orange-200 hover:text-black">
-      { text }
+    <button 
+      className={`px-10 py-2 border text-lg rounded-full w-max text-center transition duration-200 ${selected && `bg-orange-200 text-black `}`}
+      onClick={handleClick}
+      >
+      { children }
     </button>
   )
 }
--- frontend/src/pages/ChoosePage.tsx
+++ frontend/src/pages/ChoosePage.tsx
@@ -1,8 +1,11 @@
+import Adjective from "../components/Adjective";
+import { motion } from "framer-motion";
+
 const ChoosePage = () => {
   return (
-    <>
-    
-    </>
+    <motion.div className='absolute bottom-0 left-0 right-0 top-0 grid place-items-center'>
+      <Adjective>Caring</Adjective>
+    </motion.div>
   )
 }
 
